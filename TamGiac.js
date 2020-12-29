4c37061ff6725a22190cda09b2c251e4046e9fd9
Nguyễn Thị Lan <55801694+LanNguyenITUS@users.noreply.github.com>
2020-12-29 07:27:11 +0700

    git add

--- TamGiac.js
+++ TamGiac.js
@@ -11,32 +11,30 @@ module.exports = class TamGiac {
     PhanLoaiTamGiac() {
         // Tham khảo code từ https://nguyenvanhieu.vn/kiem-tra-loai-tam-giac-bang-code-c/
         // và https://quantrimang.com/cong-thuc-tinh-dien-tich-tam-giac-164018
-        
 
-            // Tính độ dài 3 cạnh của tam giác
-            const a = Diem.distance(this.diem1, this.diem2);
-            const b = Diem.distance(this.diem2, this.diem3);
-            const c = Diem.distance(this.diem3, this.diem1);
 
-            if (a > 0 && b > 0 && c > 0) { // kiểm tra 3 cạnh có dương hay không, nếu có cạnh =0 thì suy ra có ít nhất 2 điểm trùng nhau
-                if ((a < b + c) && (b < a + c) && (c < a + b)) { // kiểm tra điều kiện tam giác
-                    if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) // định lý Pytago kiểm tra tam giác vuông
-                        if (a == b || a == c || b == c) // kiểm tra điều kiện cân
-                            return { code: 1, message: "Đây là tam giác vuông cân" };
-                        else
-                            return { code: 2, message: "Đây là tam giác vuông" };
-                    else if (a == b && b == c)
-                        return { code: 3, message: "Đây là tam giác đều" }; // kiểm tra tam giác đều
-                    else if (a == b || a == c || b == c)
-                        return { code: 4, message: "Đây là tam giác cân" }; // kiểm tra tam giác cân
-                    else if (a * a > b * b + c * c || b * b > a * a + c * c || c * c > a * a + b * b)// tam giác tù
-                        return { code: 5, message: "Đây là tam giác tù" };
+        // Tính độ dài 3 cạnh của tam giác
+        const a = Diem.distance(this.diem1, this.diem2);
+        const b = Diem.distance(this.diem2, this.diem3);
+        const c = Diem.distance(this.diem3, this.diem1);
+
+        if (a > 0 && b > 0 && c > 0) { // kiểm tra 3 cạnh có dương hay không, nếu có cạnh =0 thì suy ra có ít nhất 2 điểm trùng nhau
+            if ((a < b + c) && (b < a + c) && (c < a + b)) { // kiểm tra điều kiện tam giác
+                if (a * a == (b * b + c * c) || b * b == (a * a + c * c) || c * c == (a * a + b * b)) // định lý Pytago kiểm tra tam giác vuông
+                    if (a == b || a == c || b == c) // kiểm tra điều kiện cân
+                        return { code: 1, message: "Đây là tam giác vuông cân" };
                     else
-                        return { code: 6, message: "Đây là tam giác nhọn" };// tam giác nhọn
-                }
+                        return { code: 2, message: "Đây là tam giác vuông" };
+                else if (a == b && b == c)
+                    return { code: 3, message: "Đây là tam giác đều" }; // kiểm tra tam giác đều
+                else if (a == b || a == c || b == c)
+                    return { code: 4, message: "Đây là tam giác cân" }; // kiểm tra tam giác cân
+                else if (a * a > b * b + c * c || b * b > a * a + c * c || c * c > a * a + b * b)// tam giác tù
+                    return { code: 5, message: "Đây là tam giác tù" };
+                else
+                    return { code: 6, message: "Đây là tam giác nhọn" };// tam giác nhọn
             }
-        
-        else
+        }
             return { code: 0, message: "Đây không phải là tam giác" };
     }
 
--- app.test.js
+++ app.test.js
@@ -1,95 +1,106 @@
 const Diem = require('./Diem.js');
 const TamGiac = require('./TamGiac.js');
 
-//Kiểm tra phương thức tính khoảng cách 2 điểm A, B của lớp Diem và bằng 5
-const A = new Diem(1, 5);
-const B = new Diem(1, 0);
+//Kiểm tra hàm tính khoảng cách 2 điểm
+const A = new Diem(0,0 );
+const B = new Diem(7,0);
 
 // dùng Jest để kiểm tra kết quả mong đợi: https://jestjs.io/docs/en/expect
-test('Kiểm tra phương thức tính khoảng cách 2 điểm A, B của lớp Diem và bằng 5', () => {
-    expect(Diem.distance(A, B)).toBe(5);
+test('Kiểm tra hàm tính khoảng cách 2 điểm', () => {
+    expect(Diem.distance(A, B)).toBe(7);
 });
 
 // Tam giác vuông cân 
-const p1 = new Diem(0, 0);
-const p2 = new Diem(0, 5);
-const p3 = new Diem(5, 5);
-const tri1 = new TamGiac(p1, p2, p3);
-test('Kiểm tra phương thức phân loại với tam giác vuông cân', () => {
-    expect(tri1.PhanLoaiTamGiac()).toStrictEqual({ code: 1, message: 'Đây là tam giác vuông cân' });
+const d1 = new Diem(0, 0);
+const d2 = new Diem(7, 0);
+const d3 = new Diem(7, 7);
+const tg1 = new TamGiac(d1, d2, d3);
+test('Kiểm tra hàm phân loại tam giác vuông cân', () => {
+    expect(tg1.PhanLoaiTamGiac()).toStrictEqual({ code: 1, message: 'Đây là tam giác vuông cân' });
 });
-test('Kiểm tra tam giác có vuông cân trên với chu vi là 17.071067811865476', () => {
-    expect(tri1.ChuVi()).toBe(17.071067811865476);
+test('Kiểm tra chu vi tam giác vuông cân', () => {
+    expect(tg1.ChuVi()).toBe(23.899494936611667);
 });
 
-// Tam giác vuông có chiều dài 3 cạnh là số nguyên.
-const p1_1 = new Diem(0, 3);
-const p2_1 = new Diem(4, 3);
-const p3_1 = new Diem(0, 0);
-const tri1_1 = new TamGiac(p1_1, p2_1, p3_1);
-test('Kiểm tra phương thức phân loại với tam giác vuông"', () => {
-    expect(tri1_1.PhanLoaiTamGiac()).toStrictEqual({ code: 2, message: 'Đây là tam giác vuông' });
+// Tam giác vuông
+const d4 = new Diem(0, 0);
+const d5 = new Diem(0, 7);
+const d6 = new Diem(3, 0);
+const tg2 = new TamGiac(d4, d5, d6);
+test('Kiểm tra hàm phân loại tam giác vuông', () => {
+    expect(tg2.PhanLoaiTamGiac()).toStrictEqual({ code: 2, message: 'Đây là tam giác vuông' });
 });
-test('Kiểm tra phương thức tính chu vi tam giác vuông trên có chu vi là số nguyên', () => {
-    expect(tri1_1.ChuVi()).toBe(12);
+test('Kiểm tra hàm tính chu vi tam giác vuông', () => {
+    expect(tg2.ChuVi()).toBe(17.61577310586391);
 });
 
 //Tam giác đều
-const p4 = new Diem(2, 1);
-const p5 = new Diem(7, 1);
-const p6 = new Diem(4.5, 5.33012701892);
-const tri2 = new TamGiac(p4, p5, p6);
-test('Kiểm tra phương thức phân loại với tam giác đều', () => {
-    expect(tri2.PhanLoaiTamGiac()).toStrictEqual({ code: 3, message: 'Đây là tam giác đều' });
+const d7 = new Diem(2, 1);
+const d8 = new Diem(7, 1);
+const d9 = new Diem(4.5, 5.33012701892);
+const tg3 = new TamGiac(d7,d8,d9);
+test('Kiểm tra hàm phân loại tam giác đều', () => {
+    expect(tg3.PhanLoaiTamGiac()).toStrictEqual({ code: 3, message: 'Đây là tam giác đều' });
 });
-test('Kiểm tra phương thức tính chu vi của tam giác đều', () => {
-    expect(tri2.ChuVi()).toBe(15);
+test('Kiểm tra hàm tính chu vi tam giác đều', () => {
+    expect(tg3.ChuVi()).toBe(15);
 });
 
 //Tam giác cân
-const p7 = new Diem(5, 5);
-const p8 = new Diem(10, 10);
-const p9 = new Diem(15, 5);
-const tri3 = new TamGiac(p7, p8, p9);
-test('Kiểm tra phương thức phân loại với tam giác cân', () => {
-    expect(tri3.PhanLoaiTamGiac()).toStrictEqual({ code: 4, message: 'Đây là tam giác cân' });
+const d10 = new Diem(0, 0);
+const d11 = new Diem(7, 3);
+const d12 = new Diem(14, 0);
+const tg4 = new TamGiac(d10, d11, d12);
+test('Kiểm tra hàm phân loại tam giác cân', () => {
+    expect(tg4.PhanLoaiTamGiac()).toStrictEqual({ code: 4, message: 'Đây là tam giác cân' });
 });
-test('Kiểm tra phương thức tính chu vi của tam giác cân', () => {
-    expect(tri3.ChuVi()).toBe(24.14213562373095);
+test('Kiểm tra hàm tính chu vi tam giác cân', () => {
+    expect(tg4.ChuVi()).toBe(29.231546211727817);
 });
 
 //Tam giác tù
-const p10 = new Diem(1, 1);
-const p11 = new Diem(2, 2);
-const p12 = new Diem(0, 1);
-const tri4 = new TamGiac(p10, p11, p12);
-test('Kiểm tra phương thức phân loại với tam giác tù', () => {
-    expect(tri4.PhanLoaiTamGiac()).toStrictEqual({ code: 5, message: 'Đây là tam giác tù' });
+const d13 = new Diem(1, 1);
+const d14 = new Diem(2, 2);
+const d15 = new Diem(0, 1);
+const tg5 = new TamGiac(d13, d14, d15);
+test('Kiểm tra hàm phân loại tam giác tù', () => {
+    expect(tg5.PhanLoaiTamGiac()).toStrictEqual({ code: 5, message: 'Đây là tam giác tù' });
 });
-test('Kiểm tra phương thức tính chu vi của tam giác tù', () => {
-    expect(tri4.ChuVi()).toBe(4.650281539872885);
+test('Kiểm tra hàm tính chu vi tam giác tù', () => {
+    expect(tg5.ChuVi()).toBe(4.650281539872885);
 });
 
 //Tam giác nhọn
-const p13 = new Diem(-2.5, 3);
-const p14 = new Diem(1, 1);
-const p15 = new Diem(-3, -2);
-const tri5 = new TamGiac(p13, p14, p15);
-test('Kiểm tra phương thức phân loại với tam giác nhọn', () => {
-    expect(tri5.PhanLoaiTamGiac()).toStrictEqual({ code: 6, message: 'Đây là tam giác nhọn' });
+const d16 = new Diem(-2.5, 3);
+const d17 = new Diem(1, 1);
+const d18 = new Diem(-3, -2);
+const tg6 = new TamGiac(d16, d17, d18);
+test('Kiểm tra hàm phân loại tam giác nhọn', () => {
+    expect(tg6.PhanLoaiTamGiac()).toStrictEqual({ code: 6, message: 'Đây là tam giác nhọn' });
 });
-test('Kiểm tra phương thức tính chu vi của tam giác nhọn', () => {
-    expect(tri5.ChuVi()).toBe(14.05606668470972);
+test('Kiểm tra hàm tính chu vi tam giác nhọn', () => {
+    expect(tg6.ChuVi()).toBe(14.05606668470972);
 });
 
 // Khong phai tam giac
-const p16 = new Diem(1, 1);
-const p17 = new Diem(2, 2);
-const p18 = new Diem(0, 0);
-const tri6 = new TamGiac(p16, p17, p18);
-test('Kiểm tra phương thức phân loại với trường hợp 3 cạnh không tạo thành tam giác', () => {
-    expect(tri6.PhanLoaiTamGiac()).toStrictEqual({ code: 0, message: "Đây không phải là tam giác" });
+const d19 = new Diem(0, 0);
+const d20 = new Diem(4, 4);
+const d21 = new Diem(6, 6);
+const tg7 = new TamGiac(d19, d20, d21);
+test('Kiểm tra hàm phân loại trường hợp 3 cạnh không tạo thành tam giác', () => {
+    expect(tg7.PhanLoaiTamGiac()).toStrictEqual({ code: 0, message: "Đây không phải là tam giác" });
 });
-test('Kiểm tra phương thức tính chu vi với trường hợp 3 cạnh không tạo thành tam giác', () => {
-    expect(tri6.ChuVi()).toBe("Đây không phải là tam giác");
+test('Kiểm tra hàm tính chu vi trường hợp 3 cạnh không tạo thành tam giác', () => {
+    expect(tg7.ChuVi()).toBe("Đây không phải là tam giác");
+});
+
+const d22 = new Diem(0, 0);
+const d23 = new Diem(7, 0);
+const d24 = new Diem(3, 0);
+const tg8 = new TamGiac(d22, d23, d24);
+test('Kiểm tra hàm phân loại trường hợp 3 cạnh không tạo thành tam giác', () => {
+    expect(tg8.PhanLoaiTamGiac()).toStrictEqual({code: 0, message: "Đây không phải là tam giác"});
+});
+test('Kiểm tra hàm tính chu vi trường hợp 3 cạnh không tạo thành tam giác', () => {
+    expect(tg8.ChuVi()).toBe("Đây không phải là tam giác");
 });
